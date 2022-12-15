d0004304a7ca53a3a9a8b0ac4e470c0882889f49
naymurweb <naymurweb@gmail.com>
2022-12-15 09:35:54 +0600

    firebase and route and tailwind init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn w-64 rounded-full">Button</button>
+
     </div>
   );
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNYBLB6qB8FrlhAbRF-4QcY06n-kg8k_E",
+  authDomain: "skilify-client.firebaseapp.com",
+  projectId: "skilify-client",
+  storageBucket: "skilify-client.appspot.com",
+  messagingSenderId: "148639218045",
+  appId: "1:148639218045:web:d562a4f8237b3bee2eb414",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
